6438750eb7458754f556ddff1672b941bb6d5cef
hjbenitez <humbertobenitez5760@yahoo.ca>
2022-11-16 09:50:31 -0500

    added comments for my programming illiterate comrades

--- OneMonthAtATime/Assets/CoreMechanic.cs
+++ OneMonthAtATime/Assets/CoreMechanic.cs
@@ -6,30 +6,39 @@ using TMPro;
 
 public class CoreMechanic : MonoBehaviour
 {
+    //this displays the values onto the UI
     public TextMeshProUGUI money;
     public TextMeshProUGUI mentalHealth;
     public TextMeshProUGUI academics;
 
+    //Debug buttons for each resource
+    //Money debug buttons
     public Button moneyPlus;
     public Button moneyMinus;
 
+    //Mental health debug buttons
     public Button mentalPlus;
     public Button mentalMinus;
 
+    //Academic debug buttons
     public Button academicPlus;
     public Button academicMinus;
 
-    public float moneyValue;
+    //values of each resource
+    //this is what gets changed in the script that is then referenced by the TextMeshPro
+    public float moneyValue; 
     public float mentalHealthValue;
     public float academicsValue;
 
     // Start is called before the first frame update
     void Start()
     {
+        //grabs the current values of each resource at the start
         moneyValue = float.Parse(money.text);
         mentalHealthValue = float.Parse(mentalHealth.text);
         academicsValue = float.Parse(academics.text);
 
+        //Adds events for when the buttons are clicked
         moneyPlus.onClick.AddListener(increaseMoney);
         moneyMinus.onClick.AddListener(decreaseMoney);
 
@@ -43,12 +52,14 @@ public class CoreMechanic : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //Updates the UI text to reflect the values every frame
         money.text = moneyValue.ToString();
         mentalHealth.text = mentalHealthValue.ToString();
         academics.text = academicsValue.ToString();
     }
 
 
+    //functions used to increase/decrease the value of each resource 
     void increaseMoney()
     {
         moneyValue += 20;
