bc4b42579dccd37c44064a3061d6acadfdad1800
hjbenitez <humbertobenitez5760@yahoo.ca>
2023-03-21 21:08:49 -0400

    Reworked how Victoria's PFP updates

---
+++ OneMonthAtATime/Assets/UpdateVictoria.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Fungus;
+public class UpdateVictoria : MonoBehaviour
+{
+
+    public Character victoria;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
