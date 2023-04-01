a4786cff03fcfe2d23379f243e8d56f011d10df5
Humberto Benitez <humbertobenitez5760@yahoo.ca>
2023-04-01 04:29:59 -0400

    Narrative log adjustments

--- OneMonthAtATime/Assets/OMAAT/Scripts/GameManager.cs
+++ OneMonthAtATime/Assets/OMAAT/Scripts/GameManager.cs
@@ -28,7 +28,7 @@ public class GameManager : MonoBehaviour
 
     private static int endIndex = 0;
 
-    public static int day = 0;
+    public static int day = 1;
 
     public Image mentalHealthIcon;
     public Image academicIcon;
